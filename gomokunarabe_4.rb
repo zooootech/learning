462e53226c188570349181e14829d83d2e8d0d46
zooootech <yzmisng@gmail.com>
2021-03-18 02:03:05 +0900

    Update gomokunarabe_4.rb

--- gomokunarabe_4.rb
+++ gomokunarabe_4.rb
@@ -98,3 +98,52 @@ array.each do |a|
 end
 
 puts result
+
+# -------------------------------------------------
+
+board = []
+result = 'D'
+
+# 盤面の初期化
+(1..5).each { board.push(gets.chomp.split('')) }
+
+(1..2).each do |time|
+  o = 0
+  x = 0
+
+  i = 0
+
+  if time == 1
+    j = 0
+  else
+    j = 4
+  end
+
+  (1..5).each do
+    if board[i][j] == 'O'
+      o = o + 1
+    elsif board[i][j] == 'X'
+      x = x + 1
+    else
+      break
+    end
+
+    i = i + 1
+
+    if time == 1
+      j = j + 1
+    else
+      j = j - 1
+    end
+  end
+
+  if o == 5
+    result = 'O'
+    break
+  elsif x == 5
+    result = 'X'
+    break
+  end
+end
+
+puts result
