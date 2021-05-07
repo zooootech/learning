fe0c5dd700ef0f45be4ab4d64ceae2131d940382
zooootech <yzmisng@gmail.com>
2021-05-07 23:59:27 +0900

    座標系での規則的な移動

---
+++ board_surface_9.rb
@@ -0,0 +1,100 @@
+# 開始時点の x , y 座標と移動の歩数 N が与えられます。
+# 以下の図のように時計回りに渦を巻くように移動を N 歩行った後の x , y 座標 を答えてください。
+# なお、マスの座標系は下方向が y 座標の正の向き、右方向が x 座標の正の向きとします。
+
+
+# 入力される値
+# X Y N
+
+# ・ 1 行で、開始時点の x , y 座標を表す X , Y ,移動の歩数 N が与えられます。
+
+# 入力値最終行の末尾に改行が１つ入ります。
+# 文字列は標準入力から渡されます。
+
+
+# 期待する出力
+# 1行での出力
+# ・ 移動を N 歩行った後の x , y 座標を出力してください。
+
+# x y
+
+
+# 条件
+# すべてのテストケースにおいて、以下の条件を満たします。
+# ・ -100 ≦ X, Y ≦ 100
+# ・ 0 ≦ N ≦ 100
+
+
+# 入力例1
+# 0 0 3
+
+# 出力例1
+# 0 1
+
+# 入力例2
+# 38 47 27
+
+# 出力例2
+# 41 47
+
+# -------------------------------------------------
+
+# 「歩いた歩数 steps が 深さ d - 1 のとき角に達する」という規則性を読み取ることが重要
+
+x, y, n = gets.chomp.split.map { |i| i.to_i }
+
+steps = 0  # 歩数を初期化
+total_steps = 2  # 合計歩数を予め 2 歩進めておく
+
+depth = 3  # 初回の深さ
+
+corner_count = 1  # 角に達した回数を予め 1 にしておく
+
+if n == 1
+  x += 1
+elsif n == 2
+  x += 1
+  y += 1
+  # この時点で最初の角に達していることになる
+else
+  x += 1
+  y += 1
+  # 最初の角に達している状態から繰り返し処理をスタート
+  while true
+    steps += 1
+    total_steps += 1
+    if corner_count == 0
+      y += 1
+    elsif corner_count == 1
+      x -= 1
+    elsif corner_count == 2
+      y -= 1
+    elsif corner_count == 3
+      x += 1
+    end
+
+    # 歩いた歩数が、(深さ - 1)のとき角に達する
+    if steps == (depth - 1)
+      # 角に達する度に corner_count を +1 する
+      corner_count += 1
+      steps = 0  # 歩数をリセット
+      if corner_count == 4
+        if total_steps == n
+          break
+        else
+          steps = 1
+          total_steps += 1
+          x += 1
+          corner_count = 0
+          depth += 2
+        end
+      end
+    end
+
+    if total_steps == n
+      break
+    end
+  end
+end
+
+puts "#{x} #{y}"
