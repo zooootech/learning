74916d39cd4e6c8b6f27de0711b154613a504165
zooootech <yzmisng@gmail.com>
2021-06-07 22:55:10 +0900

    Update lottery.rb

--- lottery.rb
+++ lottery.rb
@@ -85,3 +85,24 @@ array.each do |num|
     puts "blank"
   end
 end
+
+# -------------------------------------------------
+
+b = gets.chomp.to_i
+n = gets.chomp.to_i
+
+n.times do
+    query = gets.chomp.to_i
+
+    if query == b
+        puts "first"
+    elsif query == b + 1 || query == b - 1
+        puts "adjacent"
+    elsif query % 10000 == b % 10000
+        puts "second"
+    elsif query % 1000 == b % 1000
+        puts "third"
+    else
+        puts "blank"
+    end
+end
